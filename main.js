72b2f068119a01ce712b4a68624e9e9c497e8a47
tdddt <doto1116@ewhain.net>
2022-11-18 18:23:34 +0900

    change first screen

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-var i = 2;
+var i = 1;
 var num = document.getElementsByClassName("screen").length;
 
 //while (i<=num){function nextClick(){}}
 function nextClick() {
-  var hide = document.getElementById(i);
-  var show = document.getElementById(i + 1);
-  hide.style.display = "none";
-  show.style.display = "";
-  i++;
+  if (i <= num) {
+    var hide = document.getElementById(i);
+    var show = document.getElementById(i + 1);
+    hide.style.display = "none";
+    show.style.display = "";
+    i++;
+  }
 }
 
 function backClick() {
-  var hide = document.getElementById(i);
-  var show = document.getElementById(i - 1);
-  hide.style.display = "none";
-  show.style.display = "";
-  i--;
+  if (i > 0) {
+    var hide = document.getElementById(i);
+    var show = document.getElementById(i - 1);
+    hide.style.display = "none";
+    show.style.display = "";
+    i--;
+  }
 }
